a7361661a6ffd8e8c9ff952c93c82327b1eb305a
rtopolovac <155937712+rtopolovac@users.noreply.github.com>
2024-01-22 15:41:43 +0100

    Commit initial

---
+++ src/maconsoenergyuser/BDD.java
@@ -0,0 +1,63 @@
+package maconsoenergyuser;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BDD {
+
+    private Connection connection = null;
+    private Statement statement = null;
+    private ResultSet resultSet = null;
+        
+    public boolean Connexion(String url, String username, String password) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } 
+        
+        return true;
+    }
+    
+    public boolean RequeteSQL(String sql) {
+        try {                     
+            resultSet = statement.executeQuery(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } 
+        
+        return true;
+    }
+    
+    public boolean UpdateSQL(String sql) {
+        try {                     
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } 
+        
+        return true;
+    }
+    
+    public ResultSet getResultSet() { return resultSet; }
+}
+
